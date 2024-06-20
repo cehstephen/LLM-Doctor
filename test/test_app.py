a7876d1dd8fa3872e41b40c987fd280474d516f5
Stephen Tete <cehstephen@gmail.com>
2024-06-20 08:09:26 +0100

    updated test with post method

--- test/test_app.py
+++ test/test_app.py
@@ -3,30 +3,24 @@ import unittest
 import sys
 import os
 
-# Add the project root directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app import app
 
 class TestLLMApp(unittest.TestCase):
 
     def setUp(self):
-        # Create a test Flask app
         self.app = app
         self.app.config['TESTING'] = True
         self.app.config['SECRET_KEY'] = 'G@ytsfjcxvzdeo9863-1[ouGFDERA]'
-        
-        # Create a test client
         self.client = self.app.test_client()
     
-    def test_index_route(self):
-        # Send a GET request to the app index endpoint
-        response = self.app.get('/')
-        
-        # Check if the response status code is 200 (OK) or not
-        #self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.status_code, 404)
-        #self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+    def test_index_route_via_get_request(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_index_route_via_post_request(self):
+        response = self.client.post('/')
+        self.assertEqual(response.status_code, 400)
 
 
 if __name__ == '__main__':
